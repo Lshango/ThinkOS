d5bf68efd76e5e18e1b7f1e33b9e2d03eaee84a3
wanghao <wonh_thu@163.com>
2019-07-06 23:35:53 +0800

    Test different variable.

--- code/aspace.c
+++ code/aspace.c
@@ -5,11 +5,17 @@ int global;
 
 int main (){
   int local = 5;
+  int local1 = 8;
   void *p = malloc(128);
+  void *p1 = malloc(16);
   char *s = "Hello, World";
+  char *s1 = "Hello1, World";
   printf ("Address of main is %p\n", main);
   printf ("Address of global is %p\n", &global);
   printf ("Address of local is %p\n", &local);
+  printf ("Address of local1 is %p\n", &local1);
   printf ("p points to %p\n", p);
+  printf ("p1 points to %p\n", p1);
   printf ("s points to %p\n", s);
+  printf ("s1 points to %p\n", s1);
 }
